558bbce8fdd116584f53384b0de27057e1db19d5
tr15tan01 <t.varamashvili@gmail.com>
2023-07-29 23:20:31 +0400

    implemented persons
    added persons application

--- src/pages/persons/data.js
+++ src/pages/persons/data.js
@@ -21,7 +21,7 @@ export const persons = [
     job: "intern",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of ve waistcoat offal activated charcoal slow-carb marfa hell of  pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
--- src/pages/persons/index.jsx
+++ src/pages/persons/index.jsx
@@ -1,9 +1,58 @@
 import { useState } from "react";
 import { persons } from "./data";
+import "./persons.css";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 export const Persons = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = persons[index];
-  console.log(name);
-  return <h2>Persons Component</h2>;
+  const prevPerson = () => {
+    setIndex((prevIndex) => {
+      const newIndex = prevIndex - 1;
+      if (newIndex < 0) {
+        return persons.length - 1;
+      }
+      return newIndex;
+    });
+  };
+  //some different logic with modulus operator
+  const nextPerson = () => {
+    setIndex((prevIndex) => {
+      return (prevIndex + 1) % persons.length;
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * persons.length);
+    randomNumber === index
+      ? (randomNumber = index + 1)
+      : setIndex(randomNumber);
+  };
+  return (
+    <>
+      <main>
+        <article className="review">
+          <div className="img-container">
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+          <div className="button-container">
+            <button className="prev-btn" onClick={prevPerson}>
+              <FaChevronLeft />
+            </button>
+            <button className="next-btn" onClick={nextPerson}>
+              <FaChevronRight />
+            </button>
+          </div>
+          <button className="random-btn" onClick={randomPerson}>
+            surprise me
+          </button>
+        </article>
+      </main>
+    </>
+  );
 };
